1e7cf88819fc583b3b91376113eff00905870a95
Mauricio <maurip20@gmail.com>
2019-04-05 22:12:45 -0300

    Update ssh_install.rb

--- ssh_install.rb
+++ ssh_install.rb
@@ -1,7 +1,7 @@
 require 'open-uri'
 
 File.open(" Arquivo.sh", "wb") do |saved_file|
-        open("http://https://raw.githubusercontent.com/Maurip20/trabalhoDevOps/master/ssh_install.rb?token=AI1J7Ldu3XQQKG1DYapnOqskpNLy5hYyks5cp_q5wA%3D%3D", "rb") do |read_$
+        open("https://raw.githubusercontent.com/Maurip20/trabalhoDevOps/master/ssh_install.rb?token=AI1J7Ldu3XQQKG1DYapnOqskpNLy5hYyks5cp_q5wA%3D%3D", "rb") do |read_$
         saved_file.write(read_file.read)
     end
 end
